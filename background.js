c573eb620abe2bb0bdf11f9e95e4c0a3d38b0736
Chris Booth <chrisbooth83@gmail.com>
2018-04-04 17:23:06 -0500

    updating current track reference from notification

--- background.js
+++ background.js
@@ -1,27 +1,43 @@
-var listener = function(details) {
-	console.log(`loading: ${details.url}`);
-	if( details.method == "POST" ) {
-		// console.log(details.requestBody.formData);
-		var body = details.requestBody.formData;
-		// the data is now available as body.title[0]
-		// its in an array in case the same key is used multiple
-		// times in the request.  Since we will only be using
-		// each as a single element, we can safely just reference [0]
-		console.log(body);
+// Request URLs which the track update listener will capture
+var updateTrackFilters = {urls: ['*://*.totumiter.com/*']};
+// Configuration options for the track update request listener
+var updateTrackOptions = ['blocking', 'requestBody'];
+// Track object containing the current track's information
+var currentTrack = {};
+
+/**
+ * Updates the current track reference with the details provided.
+ * @param {String} details URL form encoded track information
+ */
+function updateTrack(details) {
+	// console.log(`loading: ${details.url}`);
+	if( details.method == 'POST' ) {
+		var data = details.requestBody.formData;
+		currentTrack = extractTrackFromForm(data);
+		console.log(currentTrack);
 	}
 }
-var filter = {
-	// urls: ['<all_urls>']
-	urls: ['*://*.totumiter.com/*']
-};
 
-function extractRequestData(request) {
-	console.log(request.formData);
+/**
+ * Extracts and recreates the Track object as defined by the data within the form.
+ * @param {Object} form formData object containing track information
+ */
+function extractTrackFromForm(form) {
+	var track = new Track(
+		form.title[0],
+		form.artist[0],
+		form.album[0],
+		form.artwork[0],
+		form.progress[0],
+		form.duration[0]
+	);
+	track.creationTime = new Date(form.creationTime[0]);
+	return track;
 }
 
+// Listen for update notifications
 browser.webRequest.onBeforeRequest.addListener(
-	listener,
-	filter,
-	['blocking', 'requestBody']
-);
-console.log('extension loaded');
+	updateTrack,
+	updateTrackFilters,
+	updateTrackOptions
+)
