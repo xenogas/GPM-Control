143e286a2315938170e88bcdc0183559add18ae5
Chris Booth <chrisbooth83@gmail.com>
2018-04-05 14:59:23 -0500

    definition of message types for communciation

---
+++ message.js
@@ -0,0 +1,30 @@
+class Message {
+
+	constructor(){}
+
+	get type() {
+		throw new Error('not implemented');
+	}
+
+	get data() {
+		throw new Error('not implemented');
+	}
+
+	static get Type() { 
+		return Object.freeze({
+			All:				"all-message-types",
+			Connection:			"connection",
+			Request:			"request",
+			Update:				"update",
+			Play:				"play",
+			Pause:				"pause",
+			Rewind:				"rewind",
+			Forward:			"forward",
+			RateUp:				"rate-up",
+			RateDown:			"rate-down",
+			ImFeelingLucky:		"i'm feeling lucky",
+			Test:				"test message",
+			TestToContent:		"test-to-content message"
+		});
+	}
+}
