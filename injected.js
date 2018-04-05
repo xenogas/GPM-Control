6c33bcdb1a205e4bd8ee6f8ef57df537bcb1e605
Chris Booth <chrisbooth83@gmail.com>
2018-04-05 17:23:48 -0500

    - see last commit.

--- injected.js
+++ injected.js
@@ -1,27 +1,41 @@
 Object.defineProperty(document, "hidden", { value : false});
 // Delay in milliseconds between checks to see if a track has started playing
 var preObserverUpdateInterval = 1000;
-// Observer for changes to track data
-var songInfoObserver;
 // URL to which xhr notifications of track updates will be sent
 const notificationURL = 'https://gpmc.totumiter.com';
+// References the last tick's player state
+var lastPlayerState = new MediaPlayer(-1, false);
 
 /**
  * Send the current track information to all observers
  */
 function sendTrackUpdate() {
-	// get and encode track data 
-	var track = getTrackInfo();
-	var data = Object.keys(track)
-		.map(k => `${encodeURIComponent(k)}=${encodeURIComponent(track[k])}`)
-		.join('&');
-	// console.log(track);
+	var player = getPlayerState();
 
-	// send the data via XHR
-	var xhr = new XMLHttpRequest();
-	xhr.open("POST", notificationURL);
-	xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-	xhr.send(data);
+	if( shouldSendUpdate(player) ) {
+		// encode track and player data 
+		var track = getTrackInfo();
+		// console.log(track);
+		track = encodeFormData(track);
+		player = encodeFormData(player);
+		var data = `${track}&${player}`;
+	
+		// send the data via XHR
+		var xhr = new XMLHttpRequest();
+		xhr.open("POST", notificationURL);
+		xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+		xhr.send(data);
+	}	
+}
+
+/**
+ * Encode a singly deep JSON object into a URI component for passing via XHR.
+ * @param {Object} data JSON Object to be encoded
+ */
+function encodeFormData(data) {
+	return Object.keys(data)
+		.map(k => `${encodeURIComponent(k)}=${encodeURIComponent(data[k])}`)
+		.join('&');
 }
 
 /**
@@ -40,6 +54,44 @@ function getTrackInfo() {
 	var track = new Track(title, artist, album, art, progress, duration);
 	
 	return track;
+}
+
+/**
+ * Get's the current player's play state (playing / paused) 
+ */
+function getPlayerState() {
+	var player = new MediaPlayer(
+		-1,
+		isTrackPlaying()
+	);
+	return player;
+}
+
+/**
+ * Determines if a track update should be sent by comparing the current and
+ * last tick's player states.
+ * @param {MediaPlayer} player defines the current media player's state
+ */
+function shouldSendUpdate(player) {
+	var shouldUpdate = player.isPlaying || lastPlayerState.isPlaying;
+	lastPlayerState = player;
+	return shouldUpdate;
+}
+
+/**
+ * Helper function to determine the current play state
+ */
+function isTrackPlaying() {
+	var isPlaying = false;
+
+	var audioElements = document.getElementsByTagName("audio");
+	for( var i = 0; i < audioElements.length; i++ ) {
+		if( !audioElements[i].paused ) {
+			isPlaying = true;
+		}
+	}
+	
+	return isPlaying;
 }
 
 /**
